635c27cb79fc0e123531e44aa3460e1c411d6c0f
Ilya <iosdsbb@gmail.com>
2026-04-17 17:34:20 +0300

    feat(platform): added error

--- src/platform/unsupported.rs
+++ src/platform/unsupported.rs
@@ -3,3 +3,7 @@ use crate::{HardwareInfo, HardwareInfoError, Result};
 pub(crate) fn collect() -> Result<HardwareInfo> {
     Err(HardwareInfoError::PlatformUnsupported(std::env::consts::OS))
 }
+
+pub(crate) fn collect_os_info() -> Result<crate::OsInfo> {
+    Err(HardwareInfoError::PlatformUnsupported(std::env::consts::OS))
+}
