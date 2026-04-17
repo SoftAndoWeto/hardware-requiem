593fceb02654e062c4e96b2ad83f913be8d9fe34
Ilya <iosdsbb@gmail.com>
2026-04-17 17:34:41 +0300

    feat(platform): pub collect_os_info

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -9,3 +9,9 @@ pub(crate) use self::windows::collect;
 
 #[cfg(not(windows))]
 pub(crate) use self::unsupported::collect;
+
+#[cfg(windows)]
+pub(crate) use self::windows::collect_os_info;
+
+#[cfg(not(windows))]
+pub(crate) use self::unsupported::collect_os_info;
